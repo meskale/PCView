dad4e2fb3d584f14d71d1b590d733321d893693b
CeB <CeB@CeB-PC>
2013-05-21 11:29:38 +0200

    Ajout Formulaire de contact

--- php/index.php
+++ php/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
@@ -6,55 +6,84 @@
 <LINK rel="stylesheet" type="text/css" href="UTBM/css/cssmenu.css">
 </head>
 
-<header id="header">
-	<!-- Menu -->
-	<div id="cssmenu">
-		<ul id="onglets">
-			<li class="active\"><a href="Accueil.html\"> Accueil </a></li>
-			<li><a href="Forums.html"> Forums </a></li>
-			<li><a href="Livre_or.html"> Livre d'or </a></li>
-			<li><a href="Equipe.html"> L'�quipe </a></li>
-			<li><a href="Inscription.html"> S'inscrire </a></li>
-			<li><a href="Connexion.html"> Se connecter </a></li>
-			<li class='has-sub'><a href='#'><span>Products</span> </a>
-				<ul>
-					<li><a href='#'><span>Product 1</span> </a></li>
-					<li class='last'><a href='#'><span>Product 2</span> </a></li>
-				</ul>
-			</li>
-		</ul>
-	</div>
-</header>
 
 <body>
 
+	<header id="header">
+		<!-- Menu -->
+		<div id="cssmenu">
+			<ul id="onglets">
+				<li class="active\"><a href="index.php"> Accueil </a></li>
+				<li><a href="UTBM/contact.php"> Contact </a></li>
+				<li><a href="Livre_or.html"> Livre d'or </a></li>
+				<li><a href="Equipe.html"> L'équipe </a></li>
+				<li><a href="Inscription.html"> S'inscrire </a></li>
+				<li><a href="Connexion.html"> Se connecter </a></li>
+				<li class='has-sub'><a href='#'><span>Products</span> </a>
+					<ul>
+						<li><a href='#'><span>Product 1</span> </a></li>
+						<li class='last'><a href='#'><span>Product 2</span> </a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<!-- Menu -->
+	</header>
 
 
-	<?php
-
-	require('class/FlotteParser.class.php');
-
-
-	$parser = new FlotteParser();
-	$flotte = $parser->parse();
-
-	echo "<h2>Il y a " . count($flotte->getFlotte()) . " PC dans la flotte</h2>";
+	<div id="content">
 	
-	echo "<FORM>";
-	echo "<SELECT name=\"nom\" size=\"1\">";
-	foreach ($flotte->getFlotte() as $PC) {
-		echo "<OPTION>" . $PC->getName();
-	}
-	echo "</SELECT>";
-	echo "</FORM>";
+	<section>
+		<?php
 
-	$flotte->display();
-
-	?>
+		require('class/FlotteParser.class.php');
 
 
-</body>
+		$parser = new FlotteParser();
+		$flotte = $parser->parse();
+
+		echo "<h2>Il y a " . count($flotte->getFlotte()) . " PC dans la flotte</h2>";
+
+		?>
+		</section>
+
+		<section>
+		<?php
+
+		echo "<FORM  METHOD=\"GET\" ENCTYPE=\"x-www-form-urlencoded\"> ";
+
+		echo "<SELECT ONCHANGE=\"location = this.options[this.selectedIndex].value;\" name=\"nom\" size=\"10\">";
+		foreach ($flotte->getFlotte() as $PC) {
+		$pcname = $PC->getName();
+		echo "<OPTION type=\"text\" value=\"/?pc=" ; echo $pcname . "\">" . $pcname;
+		}
+
+		echo "</SELECT>";
+		echo "</FORM>";
+
+		if (isset($_GET["pc"])) {
+			$name = urldecode($_GET["pc"]);
+			$flotte->displayByName($name);
+		}
+
+		?>
+		</section>
+
+	</div>
+	
 <footer>
-	<a href="#header" title="Haut de la page">Haut de la page</a>
+
+	<br />
+	<adress> 4 rue des Jasmins </br>
+	<span class="cp">68190</span> <span class="city">ENSISHEIM</span>
+	<p class="telmail">
+		<span class="tel"> <span class="intitule">Téléphone : </span>06 66 79 17 62 </span> 
+		</br>
+		<span class="mail"> <span class="intitule">Mail : </span>cebim68@gmail.com </span>
+	</P>
+	</adress>
+
+	<br /> <a href="#header" title="Haut de la page">Haut de la page</a>
 </footer>
+</body>
 </html>
