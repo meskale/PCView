76173ae4727623710be64f395685f0602f1a0551
CeB <CeB@CeB-PC>
2013-05-21 08:35:55 +0200

    Sortie du menu de la balise php

--- php/index.php
+++ php/index.php
@@ -1,33 +1,45 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PCView</title>
+<LINK rel=\ "stylesheet\" type=\ "text/css\" href=\"UTBM/css/cssmenu.css\">
+</head>
+<body>
 
-require('class/FlotteParser.class.php');
-
-$menuStyle = "<LINK rel=\"stylesheet\" type=\"text/css\" href=\"UTBM/css/cssmenu.css\">";
-
-echo $menuStyle;
-
-$menu = "<div id=\"cssmenu\">
+	// Menu
+	<div id=\"cssmenu\">
 		<ul id=\"onglets\">
-		<li class=\"active\"><a href=\"Accueil.html\"> Accueil </a></li>
-		<li><a href=\"Forums.html\"> Forums </a></li>
-		<li><a href=\"Livre_or.html\"> Livre d'or </a></li>
-		<li><a href=\"Equipe.html\"> L'équipe </a></li>
-		<li><a href=\"Inscription.html\"> S'inscrire </a></li>
-		<li><a href=\"Connexion.html\"> Se connecter </a></li>
-		<li class='has-sub'><a href='#'><span>Products</span></a>
-			<ul>
-				<li><a href='#'><span>Product 1</span></a></li>
-				<li class='last'><a href='#'><span>Product 2</span></a></li>
-			</ul>
-		</li>
+			<li class=\"active\"><a href=\"Accueil.html\"> Accueil </a></li>
+			<li><a href=\"Forums.html\"> Forums </a></li>
+			<li><a href=\"Livre_or.html\"> Livre d'or </a></li>
+			<li><a href=\"Equipe.html\"> L'équipe </a></li>
+			<li><a href=\"Inscription.html\"> S'inscrire </a></li>
+			<li><a href=\"Connexion.html\"> Se connecter </a></li>
+			<li class='has-sub'><a href='#'><span>Products</span> </a>
+				<ul>
+					<li><a href='#'><span>Product 1</span> </a></li>
+					<li class='last'><a href='#'><span>Product 2</span> </a></li>
+				</ul>
+			</li>
 		</ul>
-		</div>";
+	</div>
 
-echo $menu;
+	<?php
 
-$parser = new FlotteParser();
-$flotte = $parser->parse();
+	require('class/FlotteParser.class.php');
 
-$flotte->display();
 
-?>
+	$parser = new FlotteParser();
+	$flotte = $parser->parse();
+
+	$flotte->display();
+
+	?>
+
+
+</body>
+</html>
+
+
+
