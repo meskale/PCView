84ad00428af33ef7c5936935ce588f5399cd27b9
meskale <johannystrugala@free.fr>
2013-05-21 16:44:01 +0200

    ajout d'image et demarrage de la mise et page et style

--- php/consultPc.php
+++ php/consultPc.php
@@ -1,34 +1,96 @@
 <?php
 require_once 'FlotteParser.class.php';
 $flotte = new FlotteParser("Flotte.xml");
-
-echo '<?xml version="1.0" encoding="UTF-8" ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-		<html xmlns="http://www.w3.org/1999/xhtml"><head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
 ?>
+<!DOCTYPE html >
+<html>
+
+<title>PC View</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<meta charset="UTF-8" />
+</head>
+<body>
 
 
-
-<h1>Consultation du pc</h1>
-
-<table border="1">
-	<caption>
-		Details du pc
-		<?php echo $_GET["id"] ?>
-	</caption>
-	<tr>
-		<th>Disque</th>
-		<th>Taille</th>
-	</tr>
-	
-<?php
-	$stockage = $flotte->getPcById($_GET["id"])->getElementsByTagName("Stockage");
-
-	foreach($stockage as $dd){
+	<header class="header">
+		<h1>
+			Consultation du pc "
+			<?php echo $_GET["id"]; ?>
+			"
+		</h1>
+		<a href="index.php">Retour au menu</a>
+	</header>
+	<div id="content">
+		
+		<div id="config">
+		<header class="header"> Details de la config </header>
+		<table class="tab">
+			<tr>
+				<th colspan="4">Stockages</th>
+			
+			
+			<tr>
+				<th>Disque</th>
+				<th>Taille</th>
+				<th>Support</th>
+				<th>Connectique</th>
+			</tr>
+			<?php
+			$stockage = $flotte->getPcById($_GET["id"])->getElementsByTagName("Stockage");
+			foreach($stockage as $dd){
 		echo "<tr><td>".$dd->getAttribute("Nom")."</td>";
 		echo "<td>".$dd->nodeValue;
-		echo $dd->getElementsByTagName("Capacite")->item(0)->getAttribute("Unite")."</td></tr>";
+		echo $dd->getElementsByTagName("Capacite")->item(0)->getAttribute("Unite")."</td>";
+		echo "<td>".$dd->getElementsByTagName("Support")->item(0)->getAttribute("Nom")."</td>";
+		echo "<td>".$dd->getElementsByTagName("Connectique")->item(0)->getAttribute("Nom")."</td></tr>";
 }
-
 ?>
-</table>
+		</table>
+		<table class="tab">
+			<tr>
+				<th colspan="6">Interfaces</th>
+			</tr>
+			<tr>
+				<th>Nom</th>
+				<th>Type</th>
+				<th>MAC</th>
+				<th>IP</th>
+				<th>Masque</th>
+				<th>Passerelle</th>
+			</tr>
+			<?php
+			$interfaces = $flotte->getPcById($_GET["id"])->getElementsByTagName("Interface");
+			foreach($interfaces as $in){
+		echo "<tr><td>".$in->getAttribute("Nom")."</td>";
+		echo "<td>".$in->getElementsByTagName("TYPE")->item(0)->nodeValue."</td>" ;
+		echo "<td>".$in->getElementsByTagName("MAC")->item(0)->nodeValue."</td>";
+		echo "<td>".$in->getElementsByTagName("IPV4")->item(0)->nodeValue."</td>";
+		echo "<td>".$in->getElementsByTagName("SUBNET")->item(0)->nodeValue."</td>";
+		echo "<td>".$in->getElementsByTagName("GATEWAY")->item(0)->nodeValue."</td>";
+		echo "</tr>";
+}
+?>
+		</table>
+		<table class="tab">
+			<tr>
+				<th colspan="3">Peripheriques</th>
+			</tr>
+			<tr>
+			<th>Nom</th>
+			<th>Connectique</th>
+			<th>Type</th>
+
+			</tr>
+			<?php
+			$peripheriques = $flotte->getPcById($_GET["id"])->getElementsByTagName("Peripherique");
+			foreach($peripheriques as $pe){
+		echo "<tr><td>".$pe->getAttribute("Nom")."</td>";
+		echo "<td>".$pe->getElementsByTagName("Connectique")->item(0)->getAttribute("Nom")."</td>" ;
+		echo "<td>".$pe->getElementsByTagName("TYPE")->item(0)->nodeValue."</td>";
+
+		echo "</tr>";
+}
+?>
+		</table>
+		</div>
+	</div>
--- php/index.php
+++ php/index.php
@@ -1,28 +1,39 @@
 <?php
 require_once 'FlotteParser.class.php';
 $flotte = new FlotteParser("Flotte.xml");
-
-echo '<?xml version="1.0" encoding="UTF-8" ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
-
 ?>
-
+<!DOCTYPE html >
+<html>
+<meta charset="UTF-8"/>
 <title>PC View</title>
-</head><body>
-<h1>Bienvenue sur l'interface de consultation de la flotte</h1>
-
-<table>
-<tr><th>Nom des pc</th></tr>
-<tr><td><ul>
-<?php 
-$pcs =$flotte->getPcList();
-    for($i=0;$i<count($pcs)+1;$i++){
+<link rel="stylesheet" type="text/css" href="style.css" />
+<meta charset="UTF-8"/>
+</head>
+<body>
+	<header class="header">
+		<h1>Bienvenue sur l'interface de consultation de la flotte</h1>
+		
+	</header>
+	<table class="tab">
+	<tr>
+	<th>Liste de pc</th>
+	</tr>
+<tr><td>
+		<nav id="listPc">
+			<ul>
+				<?php 
+				$pcs =$flotte->getPcList();
+				for($i=0;$i<count($pcs)+1;$i++){
     	echo "<li><a href='consultPc.php?id=";
-		echo $pcs->item($i)->getAttribute("id")."'>";
-		echo $pcs->item($i)->getElementsByTagName("Nom")->item(0)->nodeValue;
-		echo "</a></li>";
+    	echo $pcs->item($i)->getAttribute("id")."'>";
+    	echo $pcs->item($i)->getElementsByTagName("Nom")->item(0)->nodeValue;
+    	echo "</a></li>";
 		}
-?>   
-</ul></td></tr></table></body></html>
+		?>
+			</ul>
+		</nav>
+		</td></tr>
+	</table>
+</body>
+</html>
 
